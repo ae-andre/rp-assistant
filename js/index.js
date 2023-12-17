f5bca5fc59af1033b445bbdb4fc3a4d558bbcb9f
dnddigest <85869787+dnddigest@users.noreply.github.com>
2023-12-16 18:13:29 -0700

    added inquirer and started question setup

--- js/index.js
+++ js/index.js
@@ -1,34 +1,58 @@
 const { OpenAI } = require("openai");
+const inquirer = require('inquirer');
 
 const openai = new OpenAI({
   apiKey: "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"
 });
 
-const userInput = "longsword";
+// const getStartedBtn = document.getElementById("#get-started")
 
-const userInput2 = "goblin";
-
-const getResponse = async () => {
-  try {
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "user",
-          content: `suggest 3 one-sentence descriptions for a ${userInput} attack against a ${userInput2}. write them in first-person perspective. make the description gory.`,
-        },
-      ],
-      temperature: 0,
-      max_tokens: 500,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    });
-
-    console.log(response.choices[0].message.content);
-  } catch (error) {
-    console.error("Error fetching response:", error);
+const questions = [
+  {
+    list: 'list',
+    name: 'typeOfAttack',
+    message: "What type of attack are you doing?",
+    choices: ['Melee', 'Ranged', 'Spell'],
+    },
+  {
+    list: 'What '
   }
-};
+]
+
+
+inquirer.prompt(questions)
+  .then(answers => {
+    const getResponse = async () => {
+      try {
+        const response = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "user",
+              content: `suggest 3 one-sentence descriptions for a ${userInput} attack against a ${userInput2}. write them in first-person perspective. make the description gory.`,
+            },
+          ],
+          temperature: 0,
+          max_tokens: 500,
+          top_p: 1.0,
+          frequency_penalty: 0.0,
+          presence_penalty: 0.0,
+        });
+    
+        console.log(response.choices[0].message.content);
+      } catch (error) {
+        console.error("Error fetching response:", error);
+      }
+    };
+  })
 
 getResponse();
+
+// function assignType() {
+  
+// }
+
+
+// getStartedBtn.addEventListener('click', function() {
+//   alert('Button Clicked!');
+// });
